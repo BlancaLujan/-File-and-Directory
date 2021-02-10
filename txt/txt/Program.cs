a36268b9f2e4c6a8b0e113963a496d3d6c20751b
BlancaLujan <77300324+BlancaLujan@users.noreply.github.com>
2021-02-10 15:46:32 -0700

    commit

---
+++ txt/txt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using txt.FileManager;
+
+namespace txt
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "TestDirectory";
+            string moveToPath = "../../TestDirectory";
+
+            FileManagerClass newManager =
+                new FileManagerClass();
+
+           //  newManager.CreateAFile(path);
+            // newManager.MoveFile(path, moveToPath);
+
+            newManager.CreateDirectory(path);
+            newManager.MoveDirectory(path, moveToPath);
+        }
+    }
+}
